63deb9c0c0c3a37f4f405a0965615cedf614631b
conanwho <christianph2@gmail.com>
2017-10-23 09:42:39 -0700

    made magic8ball to switch from back to front.
    made picture and paragraph switch.
    made fortune to randomize.

--- script.js
+++ script.js
@@ -9,19 +9,41 @@ var list = ["Yes",
 			"My reply is no",
 			"Signs point to yes",
 			"Reply hazy, try again"];
+			
+var imageState = "front";
 
+//this function outputs a random number
+function randomNum(){	
+	return Math.floor(Math.random() * 10);
+}
 
 //this function responds with a fortune
 //when click the magic 8 ball image
 function shakeMagic8Ball(){
-	//alert("test");
 	
-	//grab image by id 
-	document.getElementById("magic8ball").src="magic8ballback.jpg";
+	//if the picture is of the front side...
+	if(imageState == "front"){	
+		//grab image by id 
+		document.getElementById("magic8ball").src="magic8ballback.jpg";
 	
-	//grab the results
-	document.getElementById("results").innerHTML=list[5];
+		//grab the results
+		document.getElementById("results").innerHTML=list[randomNum()];
+		
+		imageState = "back";
+	}
+	//else, it's picture of the back side...
+	else{
+		//go back to the front picture
+		document.getElementById("magic8ball").src="magic8ballfront.jpg";
+		
+		//change results paragraph to say something to propmt the user
+		//to shake the 8 ball again
+		document.getElementById("results").innerHTML="click the magic 8 ball to tell your fortune.";
+		
+		imageState = "front";
+	}
 	
-	alert(list[5]);
+	
+	//alert(randomNum());
 	
 }
