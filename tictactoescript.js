3161228d64c598aa36b8caa7ee2960e6c052e7f5
conanwho <christianph2@gmail.com>
2017-10-30 09:41:39 -0700

    added O and X and enabled switch

--- tictactoescript.js
+++ tictactoescript.js
@@ -2,5 +2,14 @@ var character = 'X';
 
 //this function handles the onclick event
 function turn(location){
+	
+	//switch between X and O
+	if(character == 'X'){
+		character = 'O';
+	}
+	else{
+		character = 'X';
+	}
+	
 		document.getElementById(location).innerHTML = character;
 }
