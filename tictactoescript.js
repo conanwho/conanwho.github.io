fe8d3b08bd1c87c015cba19f9a96bf60423dc419
conanwho <christianph2@gmail.com>
2017-11-06 08:33:46 -0800

    disabled switching overwrite

--- tictactoescript.js
+++ tictactoescript.js
@@ -1,15 +1,25 @@
-var character = 'X';
+var character = 'O';
 
 //this function handles the onclick event
 function turn(location){
 	
-	//switch between X and O
-	if(character == 'X'){
-		character = 'O';
-	}
-	else{
-		character = 'X';
+	
+	//upadate box with character, if box is empty
+	if(document.getElementById(location).innerHTML == ""){
+		
+			//update results div with feedback
+		document.getElementById("results").innerHTML = "It's " + character + "'s turn!";
+			
+		//switch between X and O
+		if(character == 'X'){
+			character = 'O';
+		}
+		else{
+			character = 'X';
+		}
+		
+		document.getElementById(location).innerHTML = character;
 	}
 	
-		document.getElementById(location).innerHTML = character;
+	
 }
