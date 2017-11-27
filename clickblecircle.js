543417d7ed763222fdba1bf3f649da42abdfe8d5
conanwho <christianph2@gmail.com>
2017-11-27 09:40:06 -0800

    start of clickable circle. added circle and enabled hidden.

---
+++ clickblecircle.js
@@ -0,0 +1,27 @@
+
+//draws random circle in clickpanel
+function randomCircle(){
+	
+	//randomly get top positions
+	
+	
+	//randomly get left positions
+	
+	
+	//upadate positions of circle
+	
+	
+	
+}
+
+//handles onclick event
+//when a random circle is clicked in clickpanel, it disapears
+//then generates a new circle
+function clickCircle(){
+	
+	//circle is clicked, hide circle
+	document.getElementById("circle").style.visibility = "hidden";
+	
+	//generate random circle
+	
+}
