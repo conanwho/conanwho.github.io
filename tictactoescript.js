d668178ed795a64a3897d3d3bdb1aa279e5de481
conanwho <christianph2@gmail.com>
2017-11-27 08:49:21 -0800

    added reset button functions and fixed some bugs about playing when winner.

--- tictactoescript.js
+++ tictactoescript.js
@@ -1,10 +1,11 @@
 var character = 'O';
+var gameOver = false;
 
 //this function handles the onclick event
 function turn(location){
 	
 	//upadate box with character, if box is empty
-	if(document.getElementById(location).innerHTML == ""){
+	if(document.getElementById(location).innerHTML == "" && gameOver == false){
 		
 			//update results div with feedback
 		document.getElementById("results").innerHTML = "It's " + character + "'s turn!";
@@ -21,9 +22,7 @@ function turn(location){
 		document.getElementById(location).innerHTML = character;
 		//check for winner after move
 		checkWinner();
-	}
-	
-	
+	}	
 }
 
 //checks for winner
@@ -49,9 +48,27 @@ function checkWinner(){
 	   ((r1c1 == r2c2) && (r1c1 == r3c3) && r1c1 != '') ||
 	   ((r1c3 == r2c2) && (r1c3 == r3c1) && r1c3 != '') ){
 		
-		//alert("Winner!");
+		//game is over now, there's a winner
+		gameOver = true;
 		
 		//update results div
 		document.getElementById("results").innerHTML = character + " wins!";
 	}
+}
+
+//clear tic tac toe board 
+function clearAll(){
+	document.getElementById("r1c1").innerHTML="";
+	document.getElementById("r1c2").innerHTML="";
+	document.getElementById("r1c3").innerHTML="";
+	document.getElementById("r2c1").innerHTML="";
+	document.getElementById("r2c2").innerHTML="";
+	document.getElementById("r2c3").innerHTML="";
+	document.getElementById("r3c1").innerHTML="";
+	document.getElementById("r3c2").innerHTML="";
+	document.getElementById("r3c3").innerHTML="";
+	
+	//reset gameover, so players can play again
+	gameOver = false;
+	
 }
