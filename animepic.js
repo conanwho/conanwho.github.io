07cdcb6b09dbf4bb5970c819705daefe09e2633e
conanwho <christianph2@gmail.com>
2018-03-05 09:24:42 -0800

    anime list and links and attempt on onmouse (hoverpic)

---
+++ animepic.js
@@ -0,0 +1,7 @@
+function transformLink(){
+	document.getElementById("test").innerHTML="<img src='http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-11/256/red-heart.png' />";
+}
+
+function revertLink(){
+	document.getElementById("test").innerHTML="Click me";
+}
